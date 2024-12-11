5020fe6bbef2abd517e8d536a7983aa650776368
natalija-gagacheva <gagachevanatalija@gmail.com>
2024-12-11 18:03:28 +0100

    Add files

---
+++ src/main/java/mk/ukim/finki/wp/lab/WebProgrammingLabApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProgrammingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProgrammingLabApplication.class, args);
+	}
+
+}
